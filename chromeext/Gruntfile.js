fd9e0c4a9081d17aaeb6c54945bc50232775d640
melnikvitaly <melnikvitaly@gmail.com>
2013-07-11 13:21:46 +0300

    little changes

--- chromeext/Gruntfile.js
+++ chromeext/Gruntfile.js
@@ -1,4 +1,29 @@
 module.exports = function(grunt) {
-	grunt.registerTask('default', []);
+	 grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),		
+		 uglify: {
+					options: {
+					 // banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+					 compress:true
+					},
+					build: {
+						files:{
+							"build/scripts/settings.min.js":[
+								  'src/scripts/config.js',
+								  'src/scripts/providers.js',
+								  'src/scripts/utils.js'
+								  ],
+							"build/scripts/popup.min.js":[
+								  'src/scripts/config.js',
+								  'src/scripts/providers.js',
+								  'src/scripts/utils.js'
+								  ]				  
+					}
+				  }		
+			  }
+	  });
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+		
+	grunt.registerTask('default', ["uglify"]);
 }
 
